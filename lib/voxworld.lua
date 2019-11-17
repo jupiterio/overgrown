df8795248a595e0376892c26ba9a51825488f3b0
jupiterio <71483657+jupiterio@users.noreply.github.com>
2019-11-16 19:03:20 -0600

    rudimentary world rendering

---
+++ lib/voxworld.lua
@@ -0,0 +1,55 @@
+local voxworld = {}
+local cd = (...):match("(.-)[^%.]+$")
+
+local voxel = require(cd .. "voxel")
+
+function voxworld.new(mfolder)
+    local world = {}
+
+    world._folder = mfolder
+    world._items = {}
+    world._zorder = {}
+
+    return setmetatable(world, {__index = voxworld})
+end
+
+function voxworld.transformPoint(x, y, z)
+    return x, y-z*4*1.5
+end
+
+function voxworld:add(id, options)
+    options = options or {}
+    if type(options.file) ~= "string" then error("options.file is required") end
+
+    local vox = voxel.new(self._folder .. "/" .. options.file)
+
+    vox.x = options.x or 0
+    vox.y = options.y or 0
+    vox.z = options.z or 0
+    vox:rotate(options.r or 0)
+    vox:origin(options.ox or 0, options.oy or 0)
+
+    self._items[id] = vox
+    table.insert(self._zorder, vox)
+
+    return vox
+end
+
+function voxworld:move(id, x, y, z)
+    local item = self:getItem(id)
+    item.x = x
+    item.y = y
+    item.z = z
+end
+
+function voxworld:getItem(id)
+    return self._items[id] or error(id .. " does not exist")
+end
+
+function voxworld:draw()
+    for k,v in pairs(self._items) do
+        v:draw(voxworld.transformPoint(v.x, v.y, v.z))
+    end
+end
+
+return voxworld
--- main.lua
+++ main.lua
@@ -1,8 +1,23 @@
+local voxel = require("lib.voxel")
+local voxworld = require("lib.voxworld")
+
+local world
+local north, east, south, west, floor
 function love.load()
+    world = voxworld.new("assets/model")
+
+    world:add("north", { file = "kitchen-1-north.vox", ox = 50, oy = 50 })
+    world:add("east", { file = "kitchen-0-east.vox", r = math.pi/2, ox = 50, oy = 50 })
+    world:add("south", { file = "kitchen-2-south.vox", r = math.pi , ox = 50, oy = 50 })
+    world:add("west", { file = "kitchen-3-west.vox", r = 3*math.pi/2, ox = 50, oy = 50 })
+    world:add("floor", { file = "kitchen-4-floor.vox", ox = 50, oy = 50 })
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.scale(2)
+    love.graphics.translate(200, 200)
+    world:draw()
 end
