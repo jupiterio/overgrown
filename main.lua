7ffba63e193e7dbbc1364a1201d1aa65f202252b
jupiterio <71483657+jupiterio@users.noreply.github.com>
2019-11-17 11:11:43 -0600

    update main.lua to match voxel.lua changes

--- main.lua
+++ main.lua
@@ -6,18 +6,20 @@ local north, east, south, west, floor
 function love.load()
     world = voxworld.new("assets/model")
 
-    world:add("north", { file = "kitchen-1-north.vox", ox = 50, oy = 50 })
-    world:add("east", { file = "kitchen-0-east.vox", r = math.pi/2, ox = 50, oy = 50 })
-    world:add("south", { file = "kitchen-2-south.vox", r = math.pi , ox = 50, oy = 50 })
-    world:add("west", { file = "kitchen-3-west.vox", r = 3*math.pi/2, ox = 50, oy = 50 })
-    world:add("floor", { file = "kitchen-4-floor.vox", ox = 50, oy = 50 })
+    world:add("north", {file = "kitchen-1-north.vox", y=-48, z=8})
+    world:add("east", {file = "kitchen-0-east.vox", x=48, z=8})
+    --world:add("south", {file = "kitchen-2-south.vox", y=48, z=8})
+    world:add("west", {file = "kitchen-3-west.vox", x=-48, z=8})
+    world:add("floor", {file = "kitchen-4-floor.vox"})
+    world:zorder()
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.scale(2)
-    love.graphics.translate(200, 200)
+    local w,h = select(3,love.window.getSafeArea())
+    love.graphics.translate(w/2,h/2)
+    love.graphics.scale(3)
     world:draw()
 end
